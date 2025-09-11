160f970c68f0dc7de023001ea914ca5739985e0f
TacoToads <enhminecraft@gmail.com>
2025-09-11 17:36:04 +0000

    finished

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        int num = 24;
+        // System.out.println("Initial Value: " + num);
+        System.out.printf("Initial value: %d%n", num);
 
+        num = num + 1;
+        System.out.printf("number is now %d%n", num);
+    
+        num += 1;
+        System.out.printf("number is now %d%n", num);
+
+        num ++;
+        System.out.printf("number is now %d%n", num);
+
+        num --;
+        System.out.printf("number is now %d%n", num);
+         num --;
+        System.out.printf("number is now %d%n", num);
+         num --;
+        System.out.printf("number is now %d%n", num);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,18 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int num = 678;
+        
+        int truncNum = num;
+        System.out.println(truncNum % 10);
+        truncNum = truncNum / 10; 
 
+        System.out.println(truncNum % 10);
+        truncNum = truncNum / 10; 
+
+        System.out.println(truncNum % 10);
+        truncNum = truncNum / 10; 
+
+    }
+}                           
